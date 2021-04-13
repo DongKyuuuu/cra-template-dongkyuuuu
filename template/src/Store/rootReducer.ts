aa86b2d300ccfa2311ea4dff27531afad8e5cb30
Dongkyuuuu <desktop@kakao.com>
2021-04-13 11:08:22 +0900

    back

--- template/src/Store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-// import reducers..
-
-const rootReducer = combineReducers({
-  // aliasName: reducer
-});
-
-export type RootReducer = ReturnType<typeof rootReducer>;
-export default rootReducer;
